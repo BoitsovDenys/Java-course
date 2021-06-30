6fc42611b31fe4d467e3926559ec1aa0d3aed706
BoitsovDenys <boitsov.denys@gmail.com>
2021-06-30 15:22:49 +0300

    gitignore has been added

--- Lesson2-Game(GuessTheNumber)/src/main/java/Main.java
+++ Lesson2-Game(GuessTheNumber)/src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        int x = 1/0;
         Model model = new Model();
         View view = new View();
         Controller controller = new Controller(model, view);

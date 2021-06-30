a38ada3e82371e83af939a3286d0fde332f6eef9
BoitsovDenys <boitsov.denys@gmail.com>
2021-06-30 17:38:05 +0300

    Reworked the repository
    Deleted unnecessary files and renamed the projects.

--- Lesson2-Game(GuessTheNumber)/src/main/java/Main.java
+++ Lesson2-Game(GuessTheNumber)/src/main/java/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        int x = 1/0;
         Model model = new Model();
         View view = new View();
         Controller controller = new Controller(model, view);
